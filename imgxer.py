bf345befdef2b366f8cf81ac81f11743efeb35b5
enihprom <tbastian@online.de>
2019-04-02 15:49:08 +0200

    prefix name fix

--- imgxer.py
+++ imgxer.py
@@ -14,8 +14,11 @@ def extract_features(ifn,classifiers):
     im.close()
 
 def test():
-    cursor.execute("drop table imgindex;")
-    cursor.execute("drop table imgindexers;")
+    try : 
+        cursor.execute("drop table imgindex;")
+        cursor.execute("drop table imgindexers;")
+    except sql.OperationalError as oe:
+        pass
     cursor.execute("create table imgindexers(class text, description text);")
     cursor.execute("create table imgindex(name text, indexvalue float, classifier text);")
 
@@ -23,7 +26,7 @@ def test():
     for f in [fn for fn in os.listdir('test_images') if 'png' in fn]:
         extract_features(f, cset)
     for c in cset:
-        cursor.execute("insert into imgindexers values ('{}', '{}');".format(c.classifier_classname,classifier_description))
+        cursor.execute("insert into imgindexers values ('{}', '{}');".format(c.classifier_classname,c.classifier_description))
     conn.close()
 
 # TODO event (upload) based reindexing
